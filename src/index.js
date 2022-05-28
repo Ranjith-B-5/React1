9c3d27d2ee9a77d81f16a3df13b25d84129527ed
Ranjith-B-5 <84143337+Ranjith-B-5@users.noreply.github.com>
2022-05-28 23:23:28 +0530

    day1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+import "./styles.css";
+
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>HEllo</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <h1 className="hcolor" contentEditable="true">
+      HEllo
+    </h1>
+    <ul>
+      <li>item1</li>
+      <li>item2</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
