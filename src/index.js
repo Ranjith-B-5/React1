2dfd7c5da3384a26e6be4695cc45e3bea4000424
Ranjith-B-5 <84143337+Ranjith-B-5@users.noreply.github.com>
2022-06-14 20:54:35 +0530

    demo1

--- src/index.js
+++ src/index.js
@@ -3,15 +3,34 @@ import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
+const today= new Date();
+console.log(today)
+const hour =today.getHours();
+
+const style1={color:"red"};
+var daytime="";
+
+if(hour>=0 && hour<12)
+{
+ style1.color="orange";
+daytime="morning";
+}
+else if(hour>=12 && hour<=18)
+{
+  style1.color="red";
+  daytime="afternoon";
+}
+else
+{
+style1.color="blue";
+daytime="evening";
+}
+
 ReactDOM.render(
   <div>
-    <h1 className="hcolor" contentEditable="true">
-      HEllo
+    <h1 style={style1} >
+      {daytime}
     </h1>
-    <ul>
-      <li>item1</li>
-      <li>item2</li>
-    </ul>
   </div>,
   document.getElementById("root")
 );
